d4ffe6790da9c4f2ba0f56a77a9881490a64c0bd
otamay6 <sabmad69@gmail.com>
2020-03-27 17:03:30 +0900

    add dynamic segment tree

--- Data_Struct.cpp
+++ Data_Struct.cpp
@@ -46,6 +46,60 @@ template<typename T> class SegmentTree{
         return f(vl,vr);
     }
 };
+
+template<typename T>
+class dynamic_segtree{
+  private:
+    using F = std::function<T(T,T)>;
+    F f;
+    F g;
+    T ei;
+    struct node_t{
+        int L,R;
+        T val;
+        node_t *ch[2];
+        node_t(int L,int R,T val):L(L),R(R),val(val){
+            ch[0]=NULL;
+            ch[1]=NULL;
+        }
+    };
+    node_t *root;
+    node_t *update(node_t* nd,int k,T x){
+        if(nd->L==nd->R){
+            nd->val = g(nd->val,x);
+            return nd;
+        }
+        if(k<=(nd->R+nd->L)/2){
+            if(!nd->ch[0]) nd->ch[0] = new node_t(nd->L,(nd->L+nd->R)/2,ei);
+            nd->ch[0] = update(nd->ch[0],k,x);
+        }
+        else{
+            if(!nd->ch[1]) nd->ch[1] = new node_t((nd->L+nd->R)/2+1,nd->R,ei);
+            nd->ch[1] = update(nd->ch[1],k,x);
+        }
+        nd->val = f(nd->ch[0]?nd->ch[0]->val:ei,nd->ch[1]?nd->ch[1]->val:ei);
+        return nd;
+    }
+    T query(node_t *nd,int l,int r){
+        if(nd->R<l||r<nd->L) return ei;
+        if(l<=nd->L&&nd->R<=r) return nd->val;
+        return f(nd->ch[0]?query(nd->ch[0],l,r):ei,nd->ch[1]?query(nd->ch[1],l,r):ei);
+    }
+  public:
+    dynamic_segtree(F f,T ei):f(f),ei(ei){}
+    ~dynamic_segtree(){delete root;root = nullptr;}
+    void init(int n){
+        root = new node_t(0,n-1,ei);
+    }
+    void update(int k,T x,F fc){
+        g=fc;
+        update(root,k,x);
+    }
+    T query(int l,int r){
+        return query(root,l,r);
+    }
+};
+
 template<typename T,typename E>
 class LazySegmentTree{
     private:
@@ -156,6 +210,7 @@ class LazySegmentTree{
         return;
     }
 };
+
 class UnionFind
 { //UnionFind木
   private:
