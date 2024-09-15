567798274084670ebe5216cf7ac304d9d318a5e3
btyamato <55166636+btyamato@users.noreply.github.com>
2024-09-15 11:09:40 +0900

    コミット漏れ

--- Data_Struct.cpp
+++ Data_Struct.cpp
@@ -83,12 +83,16 @@ class dynamic_segtree{
             nd->val = g(nd->val,x);
             return nd;
         }
-        if(k<=(nd->R+nd->L)/2){
-            if(!nd->ch[0]) nd->ch[0] = new node_t(nd->L,(nd->L+nd->R)/2,ei);
+        // Rが奇数のとき、Lの方向に丸める
+        int r_true = nd->R + nd->L;
+        if(r_true < 0) r_true -= 1;
+        r_true /= 2;
+        if(k<=r_true){
+            if(!nd->ch[0]) nd->ch[0] = new node_t(nd->L,r_true,ei);
             nd->ch[0] = update(nd->ch[0],k,x);
         }
         else{
-            if(!nd->ch[1]) nd->ch[1] = new node_t((nd->L+nd->R)/2+1,nd->R,ei);
+            if(!nd->ch[1]) nd->ch[1] = new node_t(r_true+1,nd->R,ei);
             nd->ch[1] = update(nd->ch[1],k,x);
         }
         nd->val = f(nd->ch[0]?nd->ch[0]->val:ei,nd->ch[1]?nd->ch[1]->val:ei);
@@ -100,15 +104,24 @@ class dynamic_segtree{
         return f(nd->ch[0]?query(nd->ch[0],l,r):ei,nd->ch[1]?query(nd->ch[1],l,r):ei);
     }
   public:
-    dynamic_segtree(F f,T ei):f(f),ei(ei){}
-    ~dynamic_segtree(){delete root;root = nullptr;}
-    void init(int n){
-        root = new node_t(0,n-1,ei);
+    
+    dynamic_segtree(F f,T ei, int range_l, int range_r):f(f),ei(ei){
+        root = new node_t(range_l, range_r, ei);
     }
+    ~dynamic_segtree(){delete root;root = nullptr;}
+    /// @brief 一点更新
+    /// @param k : index
+    /// @param x  : 作用モノイド
+    /// @param fc : 作用関数 
     void update(int k,T x,F fc){
         g=fc;
         update(root,k,x);
     }
+
+    /// @brief 区間取得(閉区間)
+    /// @param l : 左端
+    /// @param r : 右端
+    /// @return l以上r以下のインデックスにおける累積
     T query(int l,int r){
         return query(root,l,r);
     }
